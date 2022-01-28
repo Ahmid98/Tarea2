771ff6552722648e3b5faa9c9c4be249b7c2cee6
Ahmid98 <71163247+Ahmid98@users.noreply.github.com>
2022-01-28 16:37:57 -0600

    Add files via upload

---
+++ 201800934_Tienda/Form1.Designer.vb
@@ -0,0 +1,312 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.C_azucar = New System.Windows.Forms.TextBox()
+        Me.C_frijol = New System.Windows.Forms.TextBox()
+        Me.C_arroz = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.pfinal = New System.Windows.Forms.TextBox()
+        Me.desc = New System.Windows.Forms.TextBox()
+        Me.pcIVA = New System.Windows.Forms.TextBox()
+        Me.IVA = New System.Windows.Forms.TextBox()
+        Me.psIVA = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.BtnAceptar = New System.Windows.Forms.Button()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.BtnLimpiar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.GroupBox1.Controls.Add(Me.C_azucar)
+        Me.GroupBox1.Controls.Add(Me.C_frijol)
+        Me.GroupBox1.Controls.Add(Me.C_arroz)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Pepper Sauce", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(480, 288)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "INGRESO DE DATOS"
+        '
+        'C_azucar
+        '
+        Me.C_azucar.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.C_azucar.Location = New System.Drawing.Point(123, 206)
+        Me.C_azucar.Name = "C_azucar"
+        Me.C_azucar.Size = New System.Drawing.Size(100, 32)
+        Me.C_azucar.TabIndex = 6
+        '
+        'C_frijol
+        '
+        Me.C_frijol.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.C_frijol.Location = New System.Drawing.Point(123, 157)
+        Me.C_frijol.Name = "C_frijol"
+        Me.C_frijol.Size = New System.Drawing.Size(100, 32)
+        Me.C_frijol.TabIndex = 5
+        '
+        'C_arroz
+        '
+        Me.C_arroz.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.C_arroz.Location = New System.Drawing.Point(123, 108)
+        Me.C_arroz.Name = "C_arroz"
+        Me.C_arroz.Size = New System.Drawing.Size(100, 32)
+        Me.C_arroz.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 212)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Azucar"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 163)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Frijol"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 114)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Arroz"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(232, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CANTIDAD DE COMPRA EN LIBRAS.."
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.GroupBox2.Controls.Add(Me.pfinal)
+        Me.GroupBox2.Controls.Add(Me.desc)
+        Me.GroupBox2.Controls.Add(Me.pcIVA)
+        Me.GroupBox2.Controls.Add(Me.IVA)
+        Me.GroupBox2.Controls.Add(Me.psIVA)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Font = New System.Drawing.Font("Pepper Sauce", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(544, 14)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(505, 324)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "RESULTADOS"
+        Me.GroupBox2.Visible = False
+        '
+        'pfinal
+        '
+        Me.pfinal.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pfinal.Location = New System.Drawing.Point(174, 260)
+        Me.pfinal.Name = "pfinal"
+        Me.pfinal.Size = New System.Drawing.Size(100, 37)
+        Me.pfinal.TabIndex = 10
+        '
+        'desc
+        '
+        Me.desc.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.desc.Location = New System.Drawing.Point(174, 204)
+        Me.desc.Name = "desc"
+        Me.desc.Size = New System.Drawing.Size(100, 32)
+        Me.desc.TabIndex = 9
+        '
+        'pcIVA
+        '
+        Me.pcIVA.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pcIVA.Location = New System.Drawing.Point(174, 149)
+        Me.pcIVA.Name = "pcIVA"
+        Me.pcIVA.Size = New System.Drawing.Size(100, 32)
+        Me.pcIVA.TabIndex = 8
+        '
+        'IVA
+        '
+        Me.IVA.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.IVA.Location = New System.Drawing.Point(174, 103)
+        Me.IVA.Name = "IVA"
+        Me.IVA.Size = New System.Drawing.Size(100, 32)
+        Me.IVA.TabIndex = 7
+        '
+        'psIVA
+        '
+        Me.psIVA.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.psIVA.Location = New System.Drawing.Point(174, 58)
+        Me.psIVA.Name = "psIVA"
+        Me.psIVA.Size = New System.Drawing.Size(100, 32)
+        Me.psIVA.TabIndex = 6
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(30, 266)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(88, 20)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "Pago final"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(30, 210)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(80, 20)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "Descuento"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(30, 155)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(102, 20)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Pago con IVA"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(30, 109)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(31, 20)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "IVA"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(30, 64)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(97, 20)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Pago sin IVA"
+        '
+        'BtnAceptar
+        '
+        Me.BtnAceptar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.BtnAceptar.Font = New System.Drawing.Font("Pepper Sauce", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnAceptar.Location = New System.Drawing.Point(221, 422)
+        Me.BtnAceptar.Name = "BtnAceptar"
+        Me.BtnAceptar.Size = New System.Drawing.Size(149, 70)
+        Me.BtnAceptar.TabIndex = 2
+        Me.BtnAceptar.Text = "ACEPTAR"
+        Me.BtnAceptar.UseVisualStyleBackColor = False
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.BackColor = System.Drawing.Color.Red
+        Me.BtnSalir.Font = New System.Drawing.Font("Pepper Sauce", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSalir.Location = New System.Drawing.Point(702, 422)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(149, 70)
+        Me.BtnSalir.TabIndex = 3
+        Me.BtnSalir.Text = "SALIR"
+        Me.BtnSalir.UseVisualStyleBackColor = False
+        '
+        'BtnLimpiar
+        '
+        Me.BtnLimpiar.BackColor = System.Drawing.Color.Yellow
+        Me.BtnLimpiar.Font = New System.Drawing.Font("Pepper Sauce", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnLimpiar.Location = New System.Drawing.Point(456, 422)
+        Me.BtnLimpiar.Name = "BtnLimpiar"
+        Me.BtnLimpiar.Size = New System.Drawing.Size(149, 70)
+        Me.BtnLimpiar.TabIndex = 4
+        Me.BtnLimpiar.Text = "LIMPIAR"
+        Me.BtnLimpiar.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global._201800934_Tienda.My.Resources.Resources.download
+        Me.ClientSize = New System.Drawing.Size(1061, 597)
+        Me.Controls.Add(Me.BtnLimpiar)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.BtnAceptar)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "SISTEMA TIENDA"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents C_azucar As TextBox
+    Friend WithEvents C_frijol As TextBox
+    Friend WithEvents C_arroz As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents BtnAceptar As Button
+    Friend WithEvents BtnSalir As Button
+    Friend WithEvents BtnLimpiar As Button
+    Friend WithEvents psIVA As TextBox
+    Friend WithEvents pfinal As TextBox
+    Friend WithEvents desc As TextBox
+    Friend WithEvents pcIVA As TextBox
+    Friend WithEvents IVA As TextBox
+End Class
---
+++ 201800934_Tienda/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnAceptar.Click
+        If C_arroz.Text <> "" And C_frijol.Text <> "" And C_azucar.Text <> "" Then
+            Const p_arroz = 2
+            Const p_frijol = 1.75
+            Const p_azucar = 2.5
+
+            Dim t_arroz As Double
+            Dim t_frijol As Double
+            Dim t_azucar As Double
+            Dim t_compra As Double
+            Dim t_iva As Double
+            Dim t_desc As Double
+
+            t_arroz = p_arroz * C_arroz.Text
+            t_frijol = p_frijol * C_frijol.Text
+            t_azucar = p_azucar * C_azucar.Text
+            t_compra = t_arroz + t_frijol + t_azucar
+            t_iva = t_compra * 0.12
+            t_desc = (t_compra + t_iva) * 0.025
+
+            'RESULTADOS A MOSTRAR ---------------------------------------------------------
+            psIVA.Text = System.Math.Round(t_compra, 2)
+            IVA.Text = System.Math.Round((t_iva), 2)
+            pcIVA.Text = System.Math.Round(t_iva + t_compra, 2)
+            desc.Text = System.Math.Round(t_desc, 2)
+            pfinal.Text = System.Math.Round(t_compra + t_iva - t_desc, 2)
+
+            GroupBox2.Visible = True
+
+        Else
+            MsgBox("Algun campo esta vacio", MsgBoxStyle.OkOnly)
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles BtnLimpiar.Click
+        C_arroz.Clear()
+        C_frijol.Clear()
+        C_azucar.Clear()
+        C_arroz.Text = 0
+        C_frijol.Text = 0
+        C_azucar.Text = 0
+        C_arroz.Focus()
+        GroupBox2.Visible = False
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        End
+
+    End Sub
+
+    Private Sub C_arroz_KeyPress(sender As Object, e As KeyPressEventArgs) Handles C_arroz.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub C_frijol_KeyPress(sender As Object, e As KeyPressEventArgs) Handles C_frijol.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub C_azucar_KeyPress(sender As Object, e As KeyPressEventArgs) Handles C_azucar.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+End Class
